482fc1286f7ca160788869c0912a9ac07bb55439
Marlon <marlon.schemberger@gmail.com>
2022-04-18 18:40:22 -0300

    Criando lista dinamica com RecyclerView

---
+++ app/src/main/java/br/com/marlon/orgs/ui/activity/MainActivity.kt
@@ -0,0 +1,31 @@
+package br.com.marlon.orgs.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.marlon.orgs.R
+import br.com.marlon.orgs.model.Produto
+import br.com.marlon.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(
+                nome = "Teste1",
+                descricao = "Desc1",
+                valor = BigDecimal("19.99")
+            ),
+            Produto(
+                nome = "Teste2",
+                descricao = "Desc2",
+                valor = BigDecimal("29.99")
+            )
+        ))
+        //recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
