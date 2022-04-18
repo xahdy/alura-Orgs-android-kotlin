04aa3fb322be56b3002f421aee3842f2f812d9d3
Marlon <marlon.schemberger@gmail.com>
2022-04-18 17:29:09 -0300

    Primeira view criada

--- app/src/main/java/br/com/marlon/orgs/MainActivity.kt
+++ app/src/main/java/br/com/marlon/orgs/MainActivity.kt
@@ -1,4 +1,15 @@
 package br.com.marlon.orgs
 
-class MainActivity {
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
