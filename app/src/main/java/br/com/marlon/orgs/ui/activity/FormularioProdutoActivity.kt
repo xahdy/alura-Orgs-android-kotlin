5bf65933b2397ebde3574912f7f620b4c9234452
Marlon <marlon.schemberger@gmail.com>
2022-04-19 11:01:47 -0300

    Implementado formulário de produtos + botão salvar.

---
+++ app/src/main/java/br/com/marlon/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,9 @@
+package br.com.marlon.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import br.com.marlon.orgs.R
+
+class FormularioProdutoActivity :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+}
--- app/src/main/java/br/com/marlon/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/marlon/orgs/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.marlon.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.marlon.orgs.R
@@ -9,10 +10,9 @@ import br.com.marlon.orgs.model.Produto
 import br.com.marlon.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
             Produto(
