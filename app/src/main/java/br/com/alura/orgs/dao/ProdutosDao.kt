bbd60f491d12b164a4d17edb8128620da006b5cb
Marlon <marlon.schemberger@gmail.com>
2022-04-20 22:47:50 -0300

    Reestilizando formulário e lista de produtos.

---
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,27 @@
+package br.com.alura.orgs.dao
+
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto) {
+        produtos.add(produto)
+    }
+
+    fun buscaTodos(): List<Produto> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada de frutas",
+                descricao = "laranja, maçãs e uvas",
+                valor = BigDecimal("19.83")
+            )
+        )
+
+    }
+
+}
---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,74 @@
+package br.com.alura.orgs.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
+import br.com.alura.orgs.dao.ProdutosDao
+import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding
+import br.com.alura.orgs.databinding.FormularioImagemBinding
+import br.com.alura.orgs.model.Produto
+import coil.load
+import java.math.BigDecimal
+
+class FormularioProdutoActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityFormularioProdutoBinding.inflate(layoutInflater)
+    }
+    private var url: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configuraBotaoSalvar()
+        binding.activityFormularioProdutoImageView.setOnClickListener {
+            val bindingFormularioImagem = FormularioImagemBinding.inflate(layoutInflater)
+            bindingFormularioImagem.formularioImagemBotaoCarregar.setOnClickListener {
+                val url = bindingFormularioImagem.formularioImagemUrl.text.toString()
+                bindingFormularioImagem.formularioImagemImageview.load(url)
+            }
+
+            AlertDialog.Builder(this)
+                .setView(bindingFormularioImagem.root)
+                .setPositiveButton("Confirmar") { _, _ ->
+                    url = bindingFormularioImagem.formularioImagemUrl.text.toString()
+                    binding.activityFormularioProdutoImageView.load(url)
+                }
+                .setNegativeButton("Cancelar") { _, _ -> }
+                .show()
+        }
+    }
+
+    private fun configuraBotaoSalvar() {
+        val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
+        val dao = ProdutosDao()
+        botaoSalvar.setOnClickListener {
+            val produtoNovo = criaProduto()
+            dao.adiciona(produtoNovo)
+            finish()
+        }
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = binding.activityFormularioProdutoNome
+        val nome = campoNome.text.toString()
+        val campoDescricao = binding.activityFormularioProdutoDescricao
+        val descricao = campoDescricao.text.toString()
+        val campoValor = binding.activityFormularioProdutoValor
+        val valorEmTexto = campoValor.text.toString()
+        val valor = if (valorEmTexto.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+
+        return Produto(
+            nome = nome,
+            descricao = descricao,
+            valor = valor,
+            imagem = url
+        )
+    }
+
+}
